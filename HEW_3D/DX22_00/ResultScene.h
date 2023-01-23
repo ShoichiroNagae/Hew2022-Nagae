2c907b2ae1059684d994c3d91c4f4ffaf2bfb40a
Shoichiro N <shonagae1514@gmail.com>
2023-01-23 21:40:20 +0900

    リザルトからゲーム終了が可能に

--- HEW_3D/DX22_00/ResultScene.h
+++ HEW_3D/DX22_00/ResultScene.h
@@ -16,11 +16,18 @@ public:
 	~ResultScene();
 
 	void CreateConstBuffer();
+	void Init();
 	void InitCamera(Camera*);
 	void Release();
 private:
 	Camera* gpCamera;
 	myPolygon* mPoly;
+
+	int selectNum;
+
+	//　ボタンの総数
+	static const int BUTTON_AMOUNT = 2;
+
 	// 名前で格納する 呼び出すときはstring型
 	std::map <std::string, ModelData> gModelManager;
 	// オブジェクトマネージャー
